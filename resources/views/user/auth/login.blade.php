18ca7904b52368bdd754f502639226199df56190
tianyong90 <412039588@qq.com>
2016-08-08 01:23:43 +0800

    登录页调整

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -13,9 +13,8 @@
   <!-- Ionicons -->
   <link href="http://cdn.bootcss.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ vendor('bower_components/AdminLTE') }}/dist/css/AdminLTE.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="{{ vendor('bower_components/AdminLTE') }}/plugins/iCheck/square/blue.css">
+  <link rel="stylesheet" href="{{ css('user/login.css') }}" />
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -24,39 +23,24 @@
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 </head>
-<body class="hold-transition login-page">
+<body class="hold-transition">
 <div class="login-box">
   <div class="login-logo">
     <img src="{{ img('user/logo.png') }}" width="320" alt="" />
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">用户登录</p>
-
     <form action="/user/login" method="post">
       {!! csrf_field() !!}
       <div class="form-group has-feedback">
-        <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="账户名">
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
+        <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="账户名">
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" name="password" placeholder="密码">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        <input type="password" class="form-control" name="password" placeholder="密码">
       </div>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> 记住我
-            </label>
-          </div>
-        </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">登录</button>
-        </div>
-        <!-- /.col -->
-      </div>
+      <button type="submit" class="btn btn-primary btn-block">登录</button>
     </form>
 
     <!-- /.social-auth-links -->
@@ -68,13 +52,11 @@
   <!-- /.login-box-body -->
 </div>
 <!-- /.login-box -->
-
+<div class="copyright"> &copy; 2016 深圳荐货联盟网络科技有限公司 <a target="_blank" href="http://www.miitbeian.gov.cn/">粤ICP备16002610-2号</a> </div>
 <!-- jQuery 2.2.3 -->
 <script src="{{ vendor('bower_components/AdminLTE') }}/plugins/jQuery/jquery-2.2.3.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="{{ vendor('bower_components/AdminLTE') }}/bootstrap/js/bootstrap.min.js"></script>
-<!-- iCheck -->
-<script src="{{ vendor('bower_components/AdminLTE') }}/plugins/iCheck/icheck.min.js"></script>
 <script>
   $(function () {
     $('input').iCheck({
